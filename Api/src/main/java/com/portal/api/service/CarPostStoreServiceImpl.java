c990b36aa76131ecc4e439a3b3bd095a759792a8
Tiago Pereira Barreto <93627992+TiagoPBarreto@users.noreply.github.com>
2023-02-22 11:24:09 -0300

    Projeto SpringBoot - Kafka

---
+++ Api/src/main/java/com/portal/api/service/CarPostStoreServiceImpl.java
@@ -0,0 +1,28 @@
+package com.portal.api.service;
+
+import com.portal.api.client.CarPostStoreClient;
+import com.portal.api.dto.CarPostDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CarPostStoreServiceImpl implements CarPostStoreService {
+    @Autowired
+    private CarPostStoreClient carPostStoreClient;
+    @Override
+    public List<CarPostDTO> getCarForSales() {
+        return carPostStoreClient.carForSalesClient();
+    }
+
+    @Override
+    public void changeCarForSale(CarPostDTO carPost, String id) {
+        carPostStoreClient.changeCarForSaleClient(carPost,id);
+    }
+
+    @Override
+    public void removeCarForSales(String id) {
+        carPostStoreClient.deleteCarForSaleClient(id);
+    }
+}
